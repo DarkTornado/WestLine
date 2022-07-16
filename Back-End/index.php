d655d4d0c80c167fd9a26e6acdcfb98b45f6375a
Dark Tornado <dt3141592@naver.com>
2022-07-17 05:47:59 +0900

    csv -> json

--- Back-End/index.php
+++ Back-End/index.php
@@ -4,23 +4,21 @@ header('Content-Type: text/html; charset=utf-8');
 $h = date('H'); 
 $min = date('i');
 $data = read_time_table();
-$data = explode("\n", $data);
-$data = parse_time_table($data);
-echo $data[0];
+var_dump($data);
 
 function read_time_table() {
 $day = date('w');
 
 /* 주말/평일 구분 */
-if($day==0||$day==6) $file_name = 'time_table_20220401_1.csv';
-else $file_name = 'time_table_20220401_0.csv';
+if($day==0||$day==6) $file_name = 'time_table_20220401_1.json';
+else $file_name = 'time_table_20220401_0.json';
 
 $fp = fopen($file_name, 'r');
 $size = filesize($file_name);
 if($size>0){
 $data = fread($fp, $size);
 fclose($fp);
-return $data;
+return json_decode($data);
 }
 else{
 echo '{"error":true,"msg":"Cannot read file"}';
