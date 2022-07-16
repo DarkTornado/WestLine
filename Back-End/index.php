29a4480fd4a033fb39faf214e56db1b322334237
Dark Tornado <dt3141592@naver.com>
2022-07-17 05:24:19 +0900

    Update index.php

--- Back-End/index.php
+++ Back-End/index.php
@@ -1,6 +1,33 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
+$h = date('H'); 
+$min = date('i');
+$data = read_time_table();
+$data = explode("\n", $data);
+$data = parse_time_table($data);
+echo $data[0];
+
+function read_time_table() {
+$day = date('w');
+
+/* 주말/평일 구분 */
+if($day==0||$day==6) $file_name = 'time_table_20220401_1.csv';
+else $file_name = 'time_table_20220401_0.csv';
+
+$fp = fopen($file_name, 'r');
+$size = filesize($file_name);
+if($size>0){
+$data = fread($fp, $size);
+fclose($fp);
+return $data;
+}
+else{
+echo '{"error":true,"msg":"Cannot read file"}';
+fclose($fp);
+exit;
+}
+}
 
 
 ?>
