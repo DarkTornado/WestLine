fdb24b681b09ff9e1a5d5f6acdb34d42d352f9ae
Dark Tornado <dt3141592@naver.com>
2023-07-06 21:31:28 +0900

    file separated

---
+++ script.js
@@ -0,0 +1,31 @@
+setInterval(() => loadData(), 10000);
+loadData();
+
+function loadData() {
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://api.darktornado.net/subway/west');
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    req.send();
+    req.onreadystatechange = function(e) {
+        if (this.readyState == 4 && req.status === 200) {
+            data = (req.responseText + '').trim();
+            if (data != '') {
+                applyData(data);
+            }
+        }
+    };
+}
+
+function applyData(data) {
+    var src = '';
+    data = JSON.parse(data);
+    data.forEach((e) => {
+        src += '<tr>';
+        src += '<td class=train>' + (e.up ? '<img src="./images/metro_up.png">' : '↑') + '</td>';
+        src += '<td class=line></td>';
+        src += '<td class=train>' + (e.down ? '<img src="./images/metro_down.png">' : '↓') + '</td>';
+        src += '<td class=station>' + e.sta + '</td>';
+        src += '</tr>';
+    });
+    document.getElementById('data').innerHTML = src;
+}
